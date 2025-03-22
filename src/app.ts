2607ef8ffac1f6f0d0b7b66317e10136d55a4bef
ozkan <ozkandnz09@gmail.com>
2025-03-22 16:28:24 +0300

    Type Assertions

--- src/app.ts
+++ src/app.ts
@@ -37,3 +37,62 @@
 // let car2: Car = "audi"
 // let car3: Car = "fiat"
 // let car4: Car = 2022
+
+//?Object
+
+// const car: {
+//   type: string;
+//   model: string;
+//   readonly year: number;
+// } = {
+//   type: "honda",
+//   model: "crv",
+//   year: 2020,
+// //   hatchback:true //compiler error
+// };
+
+// car.year = 2021 //compiler error
+// console.log(car.year)
+
+//?Intersection
+
+// type Book = {
+//     readonly book_id:number,
+//     book_name:string
+// }
+
+// type Author = {
+//     author_id:number,
+//     author_name:string
+// }
+
+// type Sales = Book & Author
+
+// let sale1 : Sales = {
+//     author_id:123,
+//     author_name:"Tolkien",
+//     book_id:456,
+//     book_name:"King",
+//     price:2000 // compiler error
+// }
+
+// sale1.book_id = 1 // compiler error
+
+//?Type Assertions
+
+// let str : unknown = "String"
+
+// console.log(str.length) //compiler error
+
+// console.log((<string>str).length)
+
+// console.log((str as string).length)
+
+// console.log(str.length)
+
+// let score : unknown = 350
+
+// console.log(score as number **2)
+
+//?Functions
+
