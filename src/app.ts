77cbb4ed7dfd4a883d80c713e55a8464f6525e2e
ozkan <ozkandnz09@gmail.com>
2025-03-22 14:43:46 +0300

    typeof

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,39 @@
-console.log("S02")
-console.log("so2")
+// console.log("S02")
+
+//?Union Type
+// let a: string | number = 0.325;
+
+// a = "hello";
+
+// a = false; // compiler error
+
+//?Type Narrowing
+
+// function checknumber(n: string | number): void {
+
+//         n.
+//     if(typeof n === "number"){
+//         n.
+//     }else{
+//         n.
+//     }
+// }
+
+//?Type Aliases
+
+// type Mark = string | number
+
+// let c : Mark="selam"
+
+// c=2
+
+// c=true //compiler error
+
+//?String Literals
+
+// type Car = "bmw" | "audi" | "mercedes" | 2022;
+
+// let car1: Car = "bmw"
+// let car2: Car = "audi"
+// let car3: Car = "fiat"
+// let car4: Car = 2022
